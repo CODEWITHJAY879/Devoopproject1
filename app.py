ff04dcff628623085b661f59f3f4f8b203666613
Your Name <jayeshni2020@gmail.com>
2025-07-07 01:29:29 +0530

    second

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import flask
 
 app = flask (__name__)
 
+@app.route("/info")
 def lwinfo()
 	return "I AM LW FROM INDIA"
+@app.route("/phone")
 def lwphone():
 	return "9823646434"
+app.run(host="0.0.0.0")
